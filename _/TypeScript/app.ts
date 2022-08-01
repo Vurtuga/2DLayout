e0cecafddb354b6a6de467d999327b01430daba1
Eng_Mohamed Khalifa <Eng_m.khalifa@outlook.com>
2022-08-02 01:48:00 +0200

    UPdate Layout

--- _/TypeScript/app.ts
+++ _/TypeScript/app.ts
@@ -61,7 +61,7 @@ type LayoutItem = {
 
 const layout : LayoutItem [] =[
     
-        {ItemType : ItemTypes.Table , ItemHeight:2, ItemWidth:2, ItemIndex: "X00", ItemPosition:{x:0,y:0},
+        {ItemType : ItemTypes.Table , ItemHeight:2, ItemWidth:2, ItemIndex: "X0", ItemPosition:{x:0,y:0},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -72,9 +72,9 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Barrier ,ItemIndex: "X01" , ItemHeight:2,ItemWidth:1, ItemPosition:{x:2,y:0}
+        {ItemType : ItemTypes.Barrier ,ItemIndex: "X1" , ItemHeight:2,ItemWidth:1, ItemPosition:{x:2,y:0}
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X02", ItemHeight:1,ItemWidth:1, ItemPosition:{x:3,y:0},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X2", ItemHeight:1,ItemWidth:1, ItemPosition:{x:3,y:0},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -85,7 +85,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X03", ItemHeight:1,ItemWidth:1, ItemPosition:{x:4,y:0},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X3", ItemHeight:1,ItemWidth:1, ItemPosition:{x:4,y:0},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -96,7 +96,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X04", ItemHeight:1,ItemWidth:1, ItemPosition:{x:5,y:0},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X4", ItemHeight:1,ItemWidth:1, ItemPosition:{x:5,y:0},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -107,7 +107,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X13" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:3,y:1},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X5" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:3,y:1},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -118,7 +118,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }     
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X14" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:4,y:1},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X6" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:4,y:1},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -129,7 +129,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }     
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X15" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:5,y:1},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X7" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:5,y:1},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -140,9 +140,11 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }     
         },
-        {ItemType : ItemTypes.Barrier ,ItemIndex: "X20" , ItemHeight:1,ItemWidth:6, ItemPosition:{x:0,y:2}
+        {ItemType : ItemTypes.FreeSpace ,ItemIndex: "X15" , ItemHeight:1,ItemWidth:1, ItemPosition:{x:0,y:2}
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X30", ItemHeight:1,ItemWidth:3, ItemPosition:{x:0,y:3},
+        {ItemType : ItemTypes.Barrier ,ItemIndex: "X8" , ItemHeight:1,ItemWidth:5, ItemPosition:{x:1,y:2}
+        },
+        {ItemType : ItemTypes.Table ,ItemIndex: "X9", ItemHeight:1,ItemWidth:3, ItemPosition:{x:0,y:3},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -153,7 +155,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X33", ItemHeight:1,ItemWidth:1, ItemPosition:{x:3,y:3},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X10", ItemHeight:1,ItemWidth:1, ItemPosition:{x:3,y:3},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -164,7 +166,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X34", ItemHeight:1,ItemWidth:1, ItemPosition:{x:4,y:3},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X11", ItemHeight:1,ItemWidth:1, ItemPosition:{x:4,y:3},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -175,7 +177,7 @@ const layout : LayoutItem [] =[
                 TableSize :TableSizes.Small
             }
         },
-        {ItemType : ItemTypes.Table ,ItemIndex: "X35", ItemHeight:1,ItemWidth:1, ItemPosition:{x:5,y:3},
+        {ItemType : ItemTypes.Table ,ItemIndex: "X12", ItemHeight:1,ItemWidth:1, ItemPosition:{x:5,y:3},
             TableObj : {
                 NoOfCustomers : 5,
                 ReservationDateTime : new Date(),
@@ -185,20 +187,26 @@ const layout : LayoutItem [] =[
                 TableState:TableStates.Available,
                 TableSize :TableSizes.Small
             }
+        },
+        {ItemType : ItemTypes.FreeSpace ,ItemIndex: "X13" , ItemHeight:1,ItemWidth:6, ItemPosition:{x:0,y:4}
+        },
+        {ItemType : ItemTypes.FreeSpace ,ItemIndex: "X14" , ItemHeight:1,ItemWidth:6, ItemPosition:{x:0,y:5}
         },
 ];
 
 function GenerateLayout(){
     console.log("Begin Generation");
     layoutTemplate = [];
-   /*  layout.forEach(renderTables);
-    console.log(layout); */
+
     layout.forEach(renderLayout);
     itemWrapper?.append(grid);
-
+    
+    for(let x =0; x < layoutTemplate.length; x++){
+        //gridAreaTemplate.concat(layoutTemplate[x]);
+        gridAreaTemplate = gridAreaTemplate + "'" + layoutTemplate[x] + "'";
+    }
     console.log("gridAreaTemplate",gridAreaTemplate);
 
-    //gridAreaTemplate = generateGridLayoutTemplate(layout);
     grid.style.gridTemplateAreas = gridAreaTemplate;
     
 }
@@ -210,67 +218,34 @@ function renderLayout(item : LayoutItem , index:number):void{
     if(item.ItemType == ItemTypes.Table){         
         let htmlItem = document.createElement('div');
         htmlItem.setAttribute("class","grid-item table");
+        htmlItem.style.gridArea = item.ItemIndex;
         updateGridLayoutTemplate(item);
         grid?.appendChild(htmlItem);
     }else if(item.ItemType == ItemTypes.Barrier){
         let htmlItem = document.createElement('div');
         htmlItem.setAttribute("class","grid-item barrier");
+        htmlItem.style.gridArea = item.ItemIndex;
         updateGridLayoutTemplate(item);
         grid?.appendChild(htmlItem);
     }else{
         let htmlItem = document.createElement('div');
         htmlItem.setAttribute("class","grid-item free-space");
+        htmlItem.style.gridArea = item.ItemIndex;
         updateGridLayoutTemplate(item);
         grid?.appendChild(htmlItem);
     }  
 }
 
-function renderRow(row: LayoutItem[],index: number): void{
-    console.log(index);
-    //let rowTemplate ="";
-    //currentRow = index;
-   // let self = this;
-    row.forEach(function(item){
-        if(item.ItemType == ItemTypes.Table){         
-            let htmlItem = document.createElement('div');
-            htmlItem.setAttribute("class","grid-item table");
-            updateGridLayoutTemplate(item);
-            /* htmlItem.style.gridArea = item.ItemIndex;
-            rowTemplate = rowTemplate.concat(item.ItemIndex," ") */
-            grid?.appendChild(htmlItem);
-        }else if(item.ItemType == ItemTypes.Barrier){
-            let htmlItem = document.createElement('div');
-            htmlItem.setAttribute("class","grid-item barrier");
-            updateGridLayoutTemplate(item);
-            /* htmlItem.style.gridArea = item.ItemIndex;
-            rowTemplate = rowTemplate.concat(item.ItemIndex," ");   */
-            grid?.appendChild(htmlItem);
-        }else{
-            let htmlItem = document.createElement('div');
-            htmlItem.setAttribute("class","grid-item free-space");
-            updateGridLayoutTemplate(item);
-            /* htmlItem.style.gridArea = item.ItemIndex;
-            rowTemplate = rowTemplate.concat(item.ItemIndex," ") */
-            grid?.appendChild(htmlItem);
-        }    
-    });
-   
-    
-    //gridAreaTemplate = gridAreaTemplate.concat("'",rowTemplate ,"'");
-   
-    
-}
-
 function updateGridLayoutTemplate(Item : LayoutItem): void{
-    
+
     for(let i = 0; i < Item.ItemWidth ;i++){
-        if(layoutTemplate[Item.ItemPosition.x] == undefined) {layoutTemplate[Item.ItemPosition.x] = ""}
-        layoutTemplate[Item.ItemPosition.x] = layoutTemplate[Item.ItemPosition.x].concat(Item.ItemIndex," "); 
+        for(let j = 0; j < Item.ItemHeight ;j++){
+            if(layoutTemplate[Item.ItemPosition.y + j] == undefined) {layoutTemplate[Item.ItemPosition.y + j] = ""}
+            layoutTemplate[Item.ItemPosition.y + j] = layoutTemplate[Item.ItemPosition.y + j].concat(Item.ItemIndex," "); 
+        }
     }
-    for(let i = 1; i < Item.ItemHeight ;i++){
-        if(layoutTemplate[Item.ItemPosition.y + i] == undefined) {layoutTemplate[Item.ItemPosition.y + i] = ""}
-        layoutTemplate[Item.ItemPosition.y + i] = layoutTemplate[Item.ItemPosition.y + i].concat(Item.ItemIndex," "); 
-    }
+    
+
     console.log("layoutTemplate",layoutTemplate);
 
 }
--- scripts/app.js
+++ scripts/app.js
@@ -40,7 +40,7 @@ var TableShapes;
     TableShapes[TableShapes["Diamond"] = 6] = "Diamond";
 })(TableShapes || (TableShapes = {}));
 const layout = [
-    { ItemType: ItemTypes.Table, ItemHeight: 2, ItemWidth: 2, ItemIndex: "X00", ItemPosition: { x: 0, y: 0 },
+    { ItemType: ItemTypes.Table, ItemHeight: 2, ItemWidth: 2, ItemIndex: "X0", ItemPosition: { x: 0, y: 0 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -51,9 +51,9 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Barrier, ItemIndex: "X01", ItemHeight: 2, ItemWidth: 1, ItemPosition: { x: 2, y: 0 }
+    { ItemType: ItemTypes.Barrier, ItemIndex: "X1", ItemHeight: 2, ItemWidth: 1, ItemPosition: { x: 2, y: 0 }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X02", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 3, y: 0 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X2", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 3, y: 0 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -64,7 +64,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X03", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 4, y: 0 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X3", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 4, y: 0 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -75,7 +75,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X04", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 5, y: 0 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X4", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 5, y: 0 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -86,7 +86,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X13", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 3, y: 1 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X5", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 3, y: 1 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -97,7 +97,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X14", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 4, y: 1 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X6", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 4, y: 1 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -108,7 +108,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X15", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 5, y: 1 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X7", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 5, y: 1 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -119,9 +119,11 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Barrier, ItemIndex: "X20", ItemHeight: 1, ItemWidth: 6, ItemPosition: { x: 0, y: 2 }
+    { ItemType: ItemTypes.FreeSpace, ItemIndex: "X15", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 0, y: 2 }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X30", ItemHeight: 1, ItemWidth: 3, ItemPosition: { x: 0, y: 3 },
+    { ItemType: ItemTypes.Barrier, ItemIndex: "X8", ItemHeight: 1, ItemWidth: 5, ItemPosition: { x: 1, y: 2 }
+    },
+    { ItemType: ItemTypes.Table, ItemIndex: "X9", ItemHeight: 1, ItemWidth: 3, ItemPosition: { x: 0, y: 3 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -132,7 +134,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X33", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 3, y: 3 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X10", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 3, y: 3 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -143,7 +145,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X34", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 4, y: 3 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X11", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 4, y: 3 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -154,7 +156,7 @@ const layout = [
             TableSize: TableSizes.Small
         }
     },
-    { ItemType: ItemTypes.Table, ItemIndex: "X35", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 5, y: 3 },
+    { ItemType: ItemTypes.Table, ItemIndex: "X12", ItemHeight: 1, ItemWidth: 1, ItemPosition: { x: 5, y: 3 },
         TableObj: {
             NoOfCustomers: 5,
             ReservationDateTime: new Date(),
@@ -164,6 +166,10 @@ const layout = [
             TableState: TableStates.Available,
             TableSize: TableSizes.Small
         }
+    },
+    { ItemType: ItemTypes.FreeSpace, ItemIndex: "X13", ItemHeight: 1, ItemWidth: 6, ItemPosition: { x: 0, y: 4 }
+    },
+    { ItemType: ItemTypes.FreeSpace, ItemIndex: "X14", ItemHeight: 1, ItemWidth: 6, ItemPosition: { x: 0, y: 5 }
     },
 ];
 function GenerateLayout() {
@@ -171,6 +177,9 @@ function GenerateLayout() {
     layoutTemplate = [];
     layout.forEach(renderLayout);
     itemWrapper === null || itemWrapper === void 0 ? void 0 : itemWrapper.append(grid);
+    for (let x = 0; x < layoutTemplate.length; x++) {
+        gridAreaTemplate = gridAreaTemplate + "'" + layoutTemplate[x] + "'";
+    }
     console.log("gridAreaTemplate", gridAreaTemplate);
     grid.style.gridTemplateAreas = gridAreaTemplate;
 }
@@ -180,57 +189,33 @@ function renderLayout(item, index) {
     if (item.ItemType == ItemTypes.Table) {
         let htmlItem = document.createElement('div');
         htmlItem.setAttribute("class", "grid-item table");
+        htmlItem.style.gridArea = item.ItemIndex;
         updateGridLayoutTemplate(item);
         grid === null || grid === void 0 ? void 0 : grid.appendChild(htmlItem);
     }
     else if (item.ItemType == ItemTypes.Barrier) {
         let htmlItem = document.createElement('div');
         htmlItem.setAttribute("class", "grid-item barrier");
+        htmlItem.style.gridArea = item.ItemIndex;
         updateGridLayoutTemplate(item);
         grid === null || grid === void 0 ? void 0 : grid.appendChild(htmlItem);
     }
     else {
         let htmlItem = document.createElement('div');
         htmlItem.setAttribute("class", "grid-item free-space");
+        htmlItem.style.gridArea = item.ItemIndex;
         updateGridLayoutTemplate(item);
         grid === null || grid === void 0 ? void 0 : grid.appendChild(htmlItem);
     }
 }
-function renderRow(row, index) {
-    console.log(index);
-    row.forEach(function (item) {
-        if (item.ItemType == ItemTypes.Table) {
-            let htmlItem = document.createElement('div');
-            htmlItem.setAttribute("class", "grid-item table");
-            updateGridLayoutTemplate(item);
-            grid === null || grid === void 0 ? void 0 : grid.appendChild(htmlItem);
-        }
-        else if (item.ItemType == ItemTypes.Barrier) {
-            let htmlItem = document.createElement('div');
-            htmlItem.setAttribute("class", "grid-item barrier");
-            updateGridLayoutTemplate(item);
-            grid === null || grid === void 0 ? void 0 : grid.appendChild(htmlItem);
-        }
-        else {
-            let htmlItem = document.createElement('div');
-            htmlItem.setAttribute("class", "grid-item free-space");
-            updateGridLayoutTemplate(item);
-            grid === null || grid === void 0 ? void 0 : grid.appendChild(htmlItem);
-        }
-    });
-}
 function updateGridLayoutTemplate(Item) {
     for (let i = 0; i < Item.ItemWidth; i++) {
-        if (layoutTemplate[Item.ItemPosition.x] == undefined) {
-            layoutTemplate[Item.ItemPosition.x] = "";
+        for (let j = 0; j < Item.ItemHeight; j++) {
+            if (layoutTemplate[Item.ItemPosition.y + j] == undefined) {
+                layoutTemplate[Item.ItemPosition.y + j] = "";
+            }
+            layoutTemplate[Item.ItemPosition.y + j] = layoutTemplate[Item.ItemPosition.y + j].concat(Item.ItemIndex, " ");
         }
-        layoutTemplate[Item.ItemPosition.x] = layoutTemplate[Item.ItemPosition.x].concat(Item.ItemIndex, " ");
-    }
-    for (let i = 0; i < Item.ItemHeight; i++) {
-        if (layoutTemplate[Item.ItemPosition.y + i] == undefined) {
-            layoutTemplate[Item.ItemPosition.y + i] = "";
-        }
-        layoutTemplate[(Item.ItemPosition.y + 1) + i] = layoutTemplate[Item.ItemPosition.y + i].concat(Item.ItemIndex, " ");
     }
     console.log("layoutTemplate", layoutTemplate);
 }
